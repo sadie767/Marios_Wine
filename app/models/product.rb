7ac167b1ee7e0d9b286a5df0884430375f0666a0
rm <biffbangpow767@yahoo.com>
2017-11-18 17:34:02 -0800

    add made in us and most reviews scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,10 @@ class Product < ActiveRecord::Base
 
   has_many :reviews
 
-  scope :three_most_recent, -> { Product.order(created_at: :asc).last(3)}
+  scope :three_most_recent, -> { Product.order(created_at: :desc).last(3)}
   scope :usa, -> { Product.where(country: "United States") }
+  # scope :most_reviews, -> {
+  #   Product.order("reviews_count DESC").limit(1) }
+    scope :most_reviews, -> {(select("products.grape, products.id, products.name, products.cost, products.country, count(reviews.id) as reviews_count").joins(:reviews).group("products.id").order("reviews_count DESC").limit(1))}
+
 end
